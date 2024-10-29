2a032ba572f1080d178d654f7b83dff91166f2f9
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-29 14:59:00 +0100

    Add new and update existing string tools

--- src/StringTools.cs
+++ src/StringTools.cs
@@ -4,6 +4,8 @@ namespace ChatAIze.Utilities;
 
 public static class StringTools
 {
+    private const int MaxStackStringLength = 256;
+
     private static readonly FrozenDictionary<char, char> transliterationMap = new Dictionary<char, char>
     {
         { 'ą', 'a' }, { 'ć', 'c' }, { 'ę', 'e' }, { 'ł', 'l' },
@@ -13,11 +15,41 @@ public static class StringTools
         { 'Ź', 'Z' }, { 'Ż', 'Z' }
     }.ToFrozenDictionary();
 
-    public static string Transliterate(this string value)
+    public static string ToAlphanumeric(this string value, SpaceHandling spaceHandling = SpaceHandling.Keep)
     {
-        Span<char> buffer = stackalloc char[value.Length];
+        Span<char> buffer = value.Length <= MaxStackStringLength ? stackalloc char[value.Length] : new char[value.Length];
+        var newLength = 0;
 
-        for (int i = 0; i < value.Length; i++)
+        for (int i = 0; i < value.Length; ++i)
+        {
+            if (char.IsLetterOrDigit(value[i]))
+            {
+                buffer[newLength++] = value[i];
+            }
+            else if (char.IsWhiteSpace(value[i]))
+            {
+                switch (spaceHandling)
+                {
+                    case SpaceHandling.Keep:
+                        buffer[newLength++] = value[i];
+                        break;
+                    case SpaceHandling.Underscore:
+                        buffer[newLength++] = '_';
+                        break;
+                    case SpaceHandling.Remove:
+                        break;
+                }
+            }
+        }
+
+        return new string(buffer[..newLength]);
+    }
+
+    public static string ToLatin(this string value)
+    {
+        Span<char> buffer = value.Length <= MaxStackStringLength ? stackalloc char[value.Length] : new char[value.Length];
+
+        for (int i = 0; i < value.Length; ++i)
         {
             buffer[i] = transliterationMap.TryGetValue(value[i], out var replacement) ? replacement : value[i];
         }
@@ -25,19 +57,70 @@ public static class StringTools
         return new string(buffer);
     }
 
-    public static string RemoveSpecialCharacters(this string value)
+    public static string ToSnakeCase(string value)
     {
-        Span<char> buffer = stackalloc char[value.Length];
-        int j = 0;
+        Span<char> buffer = value.Length <= 128 ? stackalloc char[value.Length * 2] : new char[value.Length * 2];
 
-        for (int i = 0; i < value.Length; i++)
+        var newLength = 0;
+        var actualLength = 0;
+
+        var wasPreviousUpper = false;
+        var wasPreviousUnderscore = false;
+
+        for (var i = 0; i < value.Length; ++i)
         {
-            if (char.IsLetterOrDigit(value[i]) || char.IsWhiteSpace(value[i]))
+            if (value[i] == '_' || value[i] == '-' || value[i] == '.')
             {
-                buffer[j++] = value[i];
+                if (newLength > 0 && !wasPreviousUnderscore)
+                {
+                    buffer[newLength++] = '_';
+                }
+
+                wasPreviousUnderscore = true;
+                continue;
+            }
+
+            if (char.IsWhiteSpace(value[i]))
+            {
+                if (newLength > 0 && !wasPreviousUnderscore)
+                {
+                    buffer[newLength++] = '_';
+                    wasPreviousUnderscore = true;
+                }
+
+                continue;
+            }
+
+            if (!char.IsLetterOrDigit(value[i]))
+            {
+                continue;
+            }
+
+            var character = transliterationMap.TryGetValue(value[i], out var replacement) ? replacement : value[i];
+
+            if (char.IsUpper(character))
+            {
+                if (!wasPreviousUpper && !wasPreviousUnderscore && newLength > 0)
+                {
+                    buffer[newLength++] = '_';
+                }
+
+                buffer[newLength++] = char.ToLower(character);
+
+                actualLength = newLength;
+                wasPreviousUpper = true;
+                wasPreviousUnderscore = false;
+            }
+            else
+            {
+                buffer[newLength++] = character;
+
+                actualLength = newLength;
+                wasPreviousUpper = false;
+                wasPreviousUnderscore = false;
             }
         }
 
-        return new string(buffer[..j]);
+        return new string(buffer[..actualLength]);
     }
 }
