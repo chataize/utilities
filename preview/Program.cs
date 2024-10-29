b93eb2cb772be3611b7bbe0bb0abada11fdad3b5
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-29 16:00:54 +0100

    Add ToKebabCase example and update input to contain non-latin characters

--- preview/Program.cs
+++ preview/Program.cs
@@ -1,6 +1,8 @@
 ﻿using ChatAIze.Utilities;
 
-var input = "  _  __ Hello,  World! UserID HTTP RequestExample 1234.. FIRST.second.Third  _ ";
-var output = input.ToSnakeCase();
+var input = "  _  __ Hello,  World! UserID HTTP ąĘćÓłż RequestExample 1234.. FIRST.second.Third  _ ";
+var snakeOutput = input.ToSnakeCase();
+var kebabOutput = input.ToKebabCase();
 
-Console.WriteLine(output);
+Console.WriteLine(snakeOutput);
+Console.WriteLine(kebabOutput);
