555f21f9b63d279e3c5dd59b857fb99de22d781f
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-10 16:26:16 +0100

    Add DelegateExtensions class with GetNormalizedMethodName method

---
+++ src/DelegateExtensions.cs
@@ -0,0 +1,24 @@
+namespace ChatAIze.Utilities;
+
+public static class DelegateExtensions
+{
+    public static string GetNormalizedMethodName(this Delegate @delegate)
+    {
+        var name = @delegate.Method.Name;
+
+        if (!name.Contains("<<"))
+        {
+            return name;
+        }
+
+        var start = name.IndexOf("__");
+        var end = name.IndexOf('|');
+
+        if (start < 0 || end < 0 || start >= end)
+        {
+            throw new Exception($"Unable to get normalized method name from '{name}'.");
+        }
+
+        return name[(start + 2)..end];
+    }
+}
