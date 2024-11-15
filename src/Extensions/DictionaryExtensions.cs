df8513ea4b30b4d4b256b5274654becb612a3324
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-15 21:14:32 +0100

    Add space between lines

--- src/Extensions/DictionaryExtensions.cs
+++ src/Extensions/DictionaryExtensions.cs
@@ -5,6 +5,7 @@ public static class DictionaryExtensions
     public static IDictionary<string, object> WithPlaceholderValues(this IDictionary<string, object> dictionary, params IEnumerable<KeyValuePair<string, object>> placeholders)
     {
         var newDictionary = new Dictionary<string, object>(dictionary);
+
         foreach (var key in newDictionary.Keys)
         {
             if (newDictionary[key] is string stringValue)
