cf34996a2608311841c3265eed6db14de061909e
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-11-15 21:40:58 +0100

    Add WithPlaceholderValues overload with JsonElement values

--- src/Extensions/DictionaryExtensions.cs
+++ src/Extensions/DictionaryExtensions.cs
@@ -1,8 +1,10 @@
+using System.Text.Json;
+
 namespace ChatAIze.Utilities.Extensions;
 
 public static class DictionaryExtensions
 {
-    public static IDictionary<string, object> WithPlaceholderValues(this IDictionary<string, object> dictionary, params IEnumerable<KeyValuePair<string, object>> placeholders)
+    public static Dictionary<string, object> WithPlaceholderValues(this IDictionary<string, object> dictionary, params IEnumerable<KeyValuePair<string, object>> placeholders)
     {
         var newDictionary = new Dictionary<string, object>(dictionary);
 
@@ -11,6 +13,21 @@ public static class DictionaryExtensions
             if (newDictionary[key] is string stringValue)
             {
                 newDictionary[key] = stringValue.WithPlaceholderValues(placeholders);
+            }
+        }
+
+        return newDictionary;
+    }
+
+    public static Dictionary<string, JsonElement> WithPlaceholderValues(this IDictionary<string, JsonElement> dictionary, params IEnumerable<KeyValuePair<string, object>> placeholders)
+    {
+        var newDictionary = new Dictionary<string, JsonElement>(dictionary);
+
+        foreach (var key in newDictionary.Keys)
+        {
+            if (newDictionary[key].ValueKind == JsonValueKind.String)
+            {
+                newDictionary[key] = JsonSerializer.SerializeToElement((newDictionary[key].GetString() ?? string.Empty).WithPlaceholderValues(placeholders));
             }
         }
 
